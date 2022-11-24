91c77263c93e9915ca07511826e4bbbbf745987f
Sangjoon <djm04212@naver.com>
2022-11-24 14:06:47 +0900

    저자 이름이 보이는 책 목록

--- src/main/java/com/jpa/exercise/controller/BookController.java
+++ src/main/java/com/jpa/exercise/controller/BookController.java
@@ -1,14 +1,16 @@
 package com.jpa.exercise.controller;
 
 import com.jpa.exercise.domain.Book;
+import com.jpa.exercise.domain.dto.BookResponse;
 import com.jpa.exercise.repository.BookRepository;
+import com.jpa.exercise.service.BookService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,16 +18,33 @@ import java.util.List;
 public class BookController {
 
     private final BookRepository bookRepository;
+    private final BookService bookService;
 
-    public BookController(BookRepository bookRepository) {
+//    public BookController(BookRepository bookRepository) {
+//        this.bookRepository = bookRepository;
+//    }
+
+    public BookController(BookRepository bookRepository, BookService bookService) {
         this.bookRepository = bookRepository;
+        this.bookService = bookService;
     }
+
+//    @GetMapping("")
+//    public ResponseEntity<List<Book>> list() {
+//        List<Book> bookList = bookRepository.findAll();
+//        return ResponseEntity.ok().body(bookList);
+//    }
 
     @GetMapping("")
-    public ResponseEntity<List<Book>> list() {
-        List<Book> bookList = bookRepository.findAll();
-        return ResponseEntity.ok().body(bookList);
+    public ResponseEntity<List<BookResponse>> list(Pageable pageable) {
+        return ResponseEntity.ok().body(bookService.findBooks(pageable));
     }
+
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Book> get(@PathVariable Long id) {
+//        Book book = bookRepository.findById(id).get();
+//        return ResponseEntity.ok().body(book);
+//    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> get(@PathVariable Long id) {
--- src/main/java/com/jpa/exercise/service/BookService.java
+++ src/main/java/com/jpa/exercise/service/BookService.java
@@ -1,29 +1,24 @@
 package com.jpa.exercise.service;
 
-import com.jpa.exercise.domain.Author;
 import com.jpa.exercise.domain.Book;
 import com.jpa.exercise.domain.dto.BookResponse;
-import com.jpa.exercise.repository.AuthorRepository;
 import com.jpa.exercise.repository.BookRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.swing.*;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class BookService {
 
     private final BookRepository bookRepository;
-    private final AuthorRepository authorRepository;
 
-    public BookService(BookRepository bookRepository, AuthorRepository authorRepository) {
+    public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
-        this.authorRepository = authorRepository;
     }
+
 
     public List<BookResponse> findBooks(Pageable pageable) {
         Page<Book> books = bookRepository.findAll(pageable);
